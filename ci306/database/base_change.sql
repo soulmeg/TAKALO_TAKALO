3c65d0e5699c1239853d19523d9b90f814a28b77
Wartonio <antonioandria0803@gmail.com>
2023-02-07 15:02:23 +0300

    Create base_change.sql

---
+++ ci306/database/base_change.sql
@@ -0,0 +1,76 @@
+create database e_change;
+use e_change;
+
+
+create table user(
+    id_user int primary key auto_increment,
+     user_name varchar(30),
+     user_emai varchar(30),
+     user_pwd varchar(30),
+     est_admin boolean
+);
+
+create table categorie(
+     id_categorie int primary key auto_increment,
+     nom_categorie varchar(30)
+);
+
+create table objet(
+     id_objet int primary key auto_increment,
+     id_user int,
+     id_categorie int,
+    description_text varchar(1000),
+    prix float ,
+
+    foreign key (id_user) references user(id_user),
+    foreign key (id_categorie) references categorie(id_categorie) 
+);
+
+create table photo(
+     id_photo int primary key auto_increment,
+     id_objet int,
+     nom_photo varchar(30),
+
+    foreign key (id_objet) references objet(id_objet)
+);
+
+create table echange(
+     id_echange int primary key auto_increment,
+     id_objet_choix int ,
+     id_objet_echange int,
+     date_envoie datetime,
+     date_acceptation datetime,
+
+    foreign key (id_objet_choix) references objet(id_objet),
+    foreign key (id_objet_echange) references objet(id_objet)
+);
+
+create table etat(
+     id_etat int primary key auto_increment,
+     valeur int,
+    signe varchar(30)
+);
+create table confirmation(
+     id_confirmation int primary key auto_increment,
+     id_echange int
+);
+
+
+insert into user values(null,'Megane','megane@gmail.com','meg','true');
+insert into user values(null,'Mitantsoa','mmitantsoa@gmail.com','mita','true');
+insert into user values(null,'Antonio','antonio@gmail.com','okey','false');
+insert into user values(null,'Tsiresy','tsiresy@gmail.com','tsix','false');
+
+insert into categorie values(null,'vetement');
+insert into categorie values(null,'mobilier');
+insert into categorie values(null,'electro');
+insert into categorie values(null,'piece_meca');
+
+insert into objet values(null,4,1,'chaussure nike(air force)',55.2);
+insert into objet values(null,3,2,'table manger(un table et 4 sieges avec des coussin)',122.0);
+insert into objet values(null,2,3,'smartphone samsung(galaxy s7)',105.7);
+insert into objet values(null,1,4,'filtre a essence(Bmw x5)',57.4);
+insert into objet values(null,1,1,'blouson adidas(couleur noir et de bande blanche)',43.9);
+insert into objet values(null,2,2,'ciment(2 ciment holcim)',76.1);
+insert into objet values(null,3,3,'USB(lenovo 64GB)',25.2);
+insert into objet values(null,4,4,'echapment (v8 4T)',43.8);
