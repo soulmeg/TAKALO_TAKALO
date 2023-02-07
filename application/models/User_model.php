875dfd3ae227192df5a88d1654e1e99d61ae3241
mitattends <mitantsoarajaonera@gmail.com>
2023-02-07 13:42:22 +0100

    Update User_model.php

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -2,9 +2,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User_model extends CI_Model{
-    public function inscription($user_name, $user_pwd){
-        $sql = "INSERT INTO user (user_name,user_pwd) VALUES ('%s','%s')";
-        $sql = sprintf($sql, $this->db->escape($user_name), $this->db->escape($user_pwd));
+    public function inscription($user_name,$user_email ,$user_pwd, $est_admin){
+        $sql = "INSERT INTO user (user_name,user_email,user_pwd,est_admin) VALUES ('%s','%s','%s',false)";
+        $sql = sprintf($sql,
+        $this->db->escape($user_name), 
+        $this->db->escape($user_email),
+        $this->db->escape($est_admin));
         $this->db->query($sql);
     }
 }
